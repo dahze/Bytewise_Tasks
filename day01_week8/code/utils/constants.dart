8a38d869ae7a70d719f9cf6a806a50225a4ba114
Tayyab Shehzad <tayyabshhzd@gmail.com>
2024-08-11 19:58:50 +0500

    Add files via upload

---
+++ day01_week8/code/utils/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String apiKey = 'b9d56a49ae43e94f9fa1c08febca2382';
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageUrl = 'https://image.tmdb.org/t/p/w500/';
+}
